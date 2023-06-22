b05b739bffd5ae28fe65db7d86e76143be28270c
Shivam Prasad <shivamprsd99@gmail.com>
2023-06-22 09:13:10 +0530

    minor change to test broadcast functionality

--- src/main/kotlin/com/example/Room.kt
+++ src/main/kotlin/com/example/Room.kt
@@ -35,14 +35,14 @@ class Room(val id: String) {
     fun broadcastMessage(userId: String, message: String?) {
         val messageJson = encodeToString(MessageData.serializer(), MessageData(user_id = userId, room_id = id, message = message))
         connections.forEach {
-            if(it.key != userId) {
+//            if(it.key != userId) {
                 val connection = it.value
                 if (connection.isActive) {
                     CoroutineScope(Dispatchers.IO).launch {
                         connection.send(Frame.Text(messageJson))
                     }
                 }
-            }
+//            }
         }
     }
 
